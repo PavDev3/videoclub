627caace8124b3c61735ec5c8cfdb9163881994f
Pablo <pnunfe@gmail.com>
2023-09-12 11:38:04 +0200

    inputs controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,56 @@
 import './App.css'
+import { useEffect, useRef, useState } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
-  const { movies: mappedgMovies } = useMovies()
+  const { movies } = useMovies()
+  const inputRef = useRef()
+  const [error, setError] = useState(null)
+  const [query, setQuery] = useState('')
 
+  const handleSumit = (event) => {
+    event.preventDefault()
+    console.log({ query })
+  }
+
+  const handleChange = (event) => {
+    const newQuery = event.target.value
+    if ( newQuery.starsWith(' ')) return 
+    setQuery(event.target.value)
+  }
+
+  useEffect(() => {
+    if (query === '') {
+      setError('No se puede buscar la pelicula')
+      return
+    }
+    if (query.match (/^\d+$/)) {
+      setError('No se puede buscar por un numero')
+      return
+    }
+    if (query.length < 3) {
+      setError('No se puede buscar por menos de 3 caracteres')
+      return
+    }
+    setError(null)
+  },[query])
 
   return (
     <div className='page'>
 
       <header>
         <h1 className='title'>VideoClub</h1>
-        <form className='form'> 
-          <input placeholder='The Matrix, Star Wars, Marvel...'>
+        <form className='form' onSubmit={handleSumit}> 
+          <input onChange={handleChange}value={query}placeholder='The Matrix, Star Wars, Marvel...'>
           </input>
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
 
       <main>
-        <Movies movies={mappedgMovies}/>
+        <Movies movies={movies}/>
       </main>
     </div>
   )
